3d5034fa7a9b6feeab89a9c5da1ca2a1ebc85f3f
lucacorbucci <corbuccilu@gmail.com>
2025-03-04 17:31:17 +0100

    Added support to shap

--- src/synth_xai/comparison/compute_explanations.py
+++ src/synth_xai/comparison/compute_explanations.py
@@ -1,8 +1,17 @@
+import os
+
+default_n_threads = 64
+os.environ["OPENBLAS_NUM_THREADS"] = f"{default_n_threads}"
+os.environ["MKL_NUM_THREADS"] = f"{default_n_threads}"
+os.environ["OMP_NUM_THREADS"] = f"{default_n_threads}"
+
 import argparse
+import datetime
 import random
 import signal
 import sys
 import warnings
+from collections import Counter
 from pathlib import Path
 from types import FrameType
 
@@ -12,6 +21,7 @@ import numpy as np
 import torch
 from loguru import logger
 from multiprocess import Pool
+from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.metrics import accuracy_score
 
 from synth_xai.bb_architectures import MultiClassModel, SimpleModel
@@ -20,6 +30,7 @@ from synth_xai.comparison.utils_comparison import prepare_data
 from synth_xai.explanations.explanation_utils import (
     load_bb,
     make_predictions,
+    setup_wandb,
 )
 from synth_xai.utils import (
     aix_model,
@@ -31,9 +42,9 @@ parser = argparse.ArgumentParser(description="Training")
 parser.add_argument("--dataset_name", type=str, default=None, required=True)
 parser.add_argument("--bb_path", type=str, default=None, required=True)
 parser.add_argument("--seed", type=int, default=42)
-parser.add_argument("--explainer_name", type=str, default=None, required=True)
+parser.add_argument("--explanation_type", type=str, default=None, required=True)
 parser.add_argument("--sweep", type=bool, default=False)
-parser.add_argument("--validation_seed", type=bool, default=None)
+parser.add_argument("--validation_seed", type=int, default=None)
 parser.add_argument("--num_processes", type=int, default=20, required=True)
 parser.add_argument("--store_path", type=str, default=None, required=True)
 
@@ -79,53 +90,94 @@ if __name__ == "__main__":
         class_names,
     ) = prepare_data(args=args, current_path=current_script_path)
 
+    print(f"Using validation seed {args.validation_seed}")
+    random.seed(args.validation_seed)
+    torch.manual_seed(args.validation_seed)
+    torch.cuda.manual_seed(args.validation_seed)
+    torch.cuda.manual_seed_all(args.validation_seed)
+    torch.backends.cudnn.deterministic = True
+    rng = np.random.default_rng(args.validation_seed)
+
     categorical_features = [
         train_df.columns.get_loc(col) for col in categorical_feature_names if col in train_df.columns
     ]
 
     explainer = Explainer(
         args=args,
-        train_df=train_df,
+        x_train=x_train,
         feature_names=feature_names,
         categorical_feature_names=categorical_feature_names,
         class_names=class_names,
+        model=model,
     )
     multiprocess.set_start_method("spawn")
-    num_samples = min(20000, len(test_data))
+    num_samples = min(10000, len(test_data))
 
     def explain_sample(
-        explainer: Explainer, model: aix_model, bb: SimpleModel, sample: np.ndarray, y_sample: int, num_features: int
-    ) -> tuple[list, int, int]:
-        def predict_fn(x: np.ndarray) -> np.ndarray:
-            model.model.to("cuda" if torch.cuda.is_available() else "cpu")
-            return np.array(model.predict_proba(x))
-
-        explanation, local_pred = explainer.explain_instance(sample, predict_fn, num_features=num_features)
+        explainer: Explainer,
+        model: aix_model,
+        bb: SimpleModel,
+        sample: np.ndarray,
+        y_sample: int,
+        num_features: int,
+        index: int,
+    ) -> tuple[list, int, int, list]:
+        if index % 100 == 0:
+            logger.info(f"Explaining sample {index}")
         x_sample = torch.tensor([sample])
         y_sample = torch.tensor([y_sample], dtype=torch.float32)
+
         bb.to("cuda" if torch.cuda.is_available() else "cpu")
         sample_pred_bb = make_predictions(x_sample, y_sample, bb)
-        return explanation, sample_pred_bb[0].item(), local_pred
+
+        def predict_fn(x: np.ndarray) -> np.ndarray:
+            model.model.to("cuda" if torch.cuda.is_available() else "cpu")
+            prediction = model.predict_proba(x)
+            return prediction
+
+        start_explanation_time = datetime.datetime.now()
+
+        explanation, local_pred, feat_in_the_rule = explainer.explain_instance(
+            sample, predict_fn, sample_pred_bb[0].item()
+        )
+
+        end_time = datetime.datetime.now()
+        total_time = (end_time - start_explanation_time).microseconds
+
+        return explanation, sample_pred_bb[0].item(), local_pred, feat_in_the_rule, total_time
 
     args_list = [
-        (explainer, model, bb, x_test[sample_idx], y_test[sample_idx], len(feature_names))
+        (explainer, model, bb, x_test[sample_idx], y_test[sample_idx], len(feature_names), sample_idx)
         for sample_idx in range(num_samples)
     ]
-    with Pool(args.num_processes) as pool:
+
+    with Pool(1) as pool:
         results = pool.starmap(explain_sample, args_list)
 
-    explanations, predictions_bb, local_predictions = map(list, zip(*results, strict=False))
+    explanations, predictions_bb, local_predictions, features_in_the_rule, times = map(
+        list, zip(*results, strict=False)
+    )
 
     computed_explanations = {}
     for index in range(num_samples):
-        computed_explanations[index] = (explanations[index], predictions_bb[index])
+        computed_explanations[index] = (explanations[index], predictions_bb[index], features_in_the_rule[index])
 
-    file_name = args.explainer_name + ".pkl"
+    file_name = args.explanation_type + f"_{args.validation_seed}" + ".pkl"
     store_path = Path(args.store_path) / file_name
     with Path(store_path).open("wb") as f:
         # store the computed_explanations dictionary as a json file
         dill.dump(computed_explanations, f)
 
     fidelity = accuracy_score(predictions_bb, local_predictions)
-
+    print(Counter(predictions_bb), Counter(local_predictions))
+    wandb_run = setup_wandb(args, project_name="comparison_tango", num_samples=num_samples)
+    wandb_run.log(
+        {
+            "fidelity": fidelity,
+            "Total Time": np.mean(times),
+            "Total Time Std": np.std(times),
+            "Total Time (sec)": np.mean(times) / 1e6,
+            "Total Time Std (sec)": np.std(times) / 1e6,
+        }
+    )
     logger.info(f"Final Fidelity: {fidelity}")
--- src/synth_xai/comparison/explainer.py
+++ src/synth_xai/comparison/explainer.py
@@ -1,48 +1,95 @@
 import argparse
+import re
+import typing
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import shap
+import torch
 from lime.lime_tabular import LimeTabularExplainer
+from sklearn.cluster import KMeans
+
+from synth_xai.bb_architectures import MultiClassModel, SimpleModel
+from synth_xai.explanations.explanation_utils import load_bb
+from synth_xai.utils import prepare_adult
 
 
 class Explainer:
     def __init__(
         self,
         args: argparse.Namespace,
-        train_df: pd.DataFrame,
+        x_train: pd.DataFrame,
         feature_names: list[str],
         categorical_feature_names: list[str],
         class_names: list[str],
+        model: torch.nn.Module = None,
+        k_means_k: int = 20,
     ) -> None:
-        self.explainer_name = args.explainer_name
-        match args.explainer_name:
+        self.explanation_type = args.explanation_type
+        self.feature_names = feature_names
+        self.model = model
+        self.model.model.to("cuda" if torch.cuda.is_available() else "cpu")
+        match args.explanation_type:
             case "lime":
                 # Initialize LimeTabularExplainer
                 self.explainer = LimeTabularExplainer(
-                    train_df.drop(columns=["income_binary"]).values,  # Unscaled training data
+                    x_train,  # Unscaled training data
                     mode="classification",
                     feature_names=feature_names,
                     categorical_features=categorical_feature_names,
                     class_names=class_names,
                     discretize_continuous=True,  # Discretize continuous features for better interpretability
+                    random_state=args.validation_seed,
+                )
+            case "shap":
+                self.explainer = shap.KernelExplainer(
+                    model.predict_proba,
+                    data=shap.kmeans(x_train, k_means_k),
                 )
 
     def explain_instance(
         self,
         instance: pd.Series,
-        predict_fn: callable,
-        num_features: int,
-    ) -> list:
-        match self.explainer_name:
+        predict_fn: typing.Callable,
+        prediction_bb: int = None,
+    ) -> tuple[list, int, list]:
+        match self.explanation_type:
             case "lime":
                 # Explain instance using LimeTabularExplainer
                 explanation = self.explainer.explain_instance(
                     instance,
                     predict_fn,
-                    num_features=10,
                 )
+                feature_names = [feature for feature, weight in explanation.as_list()]
+                clean_features = [re.sub(r"[<>]=?|\d+(\.\d+)?", "", feature).strip() for feature in feature_names]
 
-                return explanation.as_list(), int(np.argmax(explanation.local_pred))
+                local_pred = 0 if explanation.local_pred[0] < 0.5 else 1
+                return explanation.as_list(), local_pred, clean_features
+            case "shap":
+                # Explain instance using SHAP
+                shap_values = self.explainer(instance)
+                # try:
+                # print(shap_values.values)
+                # print(len(shap_values.values))
+                # print(instance)
+                # print(shap_values.values[0])
+                # print()
+
+                # print(dkhjdb)
+                feature_importance = shap_values.values[:, prediction_bb]
+                # print(shap_values.values)
+
+                # except IndexError as e:
+                #     print(f"Error: {e}, {prediction_bb}")
+                #     # print(shap_values.values)
+                #     # print(prediction_bb)
+
+                return (
+                    list(zip(self.feature_names, feature_importance)),
+                    prediction_bb,
+                    self.feature_names,
+                )
             case _:
                 raise ValueError("Invalid explainer name")
                 return []
