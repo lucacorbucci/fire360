0cdac658eff6bb95b5798ab29ad7ce593b63f993
lucacorbucci <corbuccilu@gmail.com>
2025-03-05 09:17:45 +0100

    ✨ shap experiments

---
+++ src/experiments/comparison/letter/shap.sh
@@ -0,0 +1,4 @@
+for i in $(seq 1 2);
+do
+    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name letter --bb_path /home/lcorbucci/synth_xai/artifacts/letter/bb/letter_BB.pth --explanation_type shap --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/letter/comparison_explanation/shap/  --validation_seed 1
+done
---
+++ src/experiments/comparison/shuttle/shap.sh
@@ -0,0 +1,4 @@
+for i in $(seq 1 2);
+do
+    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name shuttle --bb_path /home/lcorbucci/synth_xai/artifacts/shuttle/bb/shuttle_BB.pth --explanation_type shap --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/shuttle/comparison_explanation/shap/  --validation_seed 1
+done
