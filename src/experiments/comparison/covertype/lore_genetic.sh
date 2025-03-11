418c29a40ac0e887c94af5bb0ccf75ac1609b6dd
lucacorbucci <corbuccilu@gmail.com>
2025-03-11 09:04:27 +0000

    ✨ experiments with lore genetic

---
+++ src/experiments/comparison/covertype/lore_genetic.sh
@@ -0,0 +1,5 @@
+
+for i in $(seq 5 6);
+do
+    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name covertype --bb_path /home/lcorbucci/synth_xai/artifacts/covertype/bb/covertype_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/covertype/comparison_explanation/lore/ --validation_seed $i --lore_generator genetic --num_explained_instances 10000
+done
--- src/experiments/comparison/shuttle/lore_genetic.sh
+++ src/experiments/comparison/shuttle/lore_genetic.sh
@@ -1,5 +1,5 @@
 
-for i in $(seq 5 6);
+for i in $(seq 5 5);
 do
     uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name shuttle --bb_path /home/lcorbucci/synth_xai/artifacts/shuttle/bb/shuttle_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/shuttle/comparison_explanation/lore/ --validation_seed $i --lore_generator genetic
 done
