8a773440926a98a7f0558a20c85156f45e37d498
lucacorbucci <corbuccilu@gmail.com>
2025-03-12 22:59:26 +0100

    ✨ new config

--- src/experiments/comparison/adult/lore.sh
+++ src/experiments/comparison/adult/lore.sh
@@ -1,5 +1,15 @@
 
-for i in $(seq 11 12);
+# for i in $(seq 11 12);
+# do
+#     uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name adult --bb_path /home/lcorbucci/synth_xai/artifacts/adult/bb/adult_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/adult/comparison_explanation/lore/ --validation_seed $i
+# done
+
+for i in $(seq 50 51);
 do
-    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name adult --bb_path /home/lcorbucci/synth_xai/artifacts/adult/bb/adult_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/adult/comparison_explanation/lore/ --validation_seed $i
+    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name adult --bb_path /home/lcorbucci/synth_xai/artifacts/adult/bb/adult_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/adult/comparison_explanation/lore/ --validation_seed $i  --neigh_size 1000
+done
+
+for i in $(seq 52 53);
+do
+    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name adult --bb_path /home/lcorbucci/synth_xai/artifacts/adult/bb/adult_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/adult/comparison_explanation/lore/ --validation_seed $i --neigh_size 2500
 done
--- src/experiments/comparison/shuttle/lore.sh
+++ src/experiments/comparison/shuttle/lore.sh
@@ -1,5 +1,17 @@
 
-for i in $(seq 11 12);
+# for i in $(seq 11 12);
+# do
+#     uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name shuttle --bb_path /home/lcorbucci/synth_xai/artifacts/shuttle/bb/shuttle_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/shuttle/comparison_explanation/lore/ --validation_seed $i
+# done
+
+
+for i in $(seq 50 51);
 do
-    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name shuttle --bb_path /home/lcorbucci/synth_xai/artifacts/shuttle/bb/shuttle_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/shuttle/comparison_explanation/lore/ --validation_seed $i
+    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name shuttle --bb_path /home/lcorbucci/synth_xai/artifacts/shuttle/bb/shuttle_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/shuttle/comparison_explanation/lore/ --validation_seed $i  --neigh_size 1000
+done
+
+
+for i in $(seq 52 53);
+do
+    uv run python ../../../synth_xai/comparison/compute_explanations.py --dataset_name shuttle --bb_path /home/lcorbucci/synth_xai/artifacts/shuttle/bb/shuttle_BB.pth --explanation_type lore --num_processes 20 --store_path /home/lcorbucci/synth_xai/artifacts/shuttle/comparison_explanation/lore/ --validation_seed $i  --neigh_size 2500
 done
