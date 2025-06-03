7d657d5e52566063b0d458c099b93346a6d9f0af
lucacorbucci <corbuccilu@gmail.com>
2025-06-03 15:57:19 +0200

    ✨ Added scripts to run the experiments

---
+++ src/experiments/train_explanations/dutch/run_all_2.sh
@@ -0,0 +1,4 @@
+# sh test_dt.sh
+# sh test_svm.sh
+sh test_lr.sh
+sh test_knn.sh
--- src/experiments/train_explanations/dutch/test_lr.sh
+++ src/experiments/train_explanations/dutch/test_lr.sh
@@ -1,8 +1,8 @@
 
-# for i in $(seq 1 2);
-# do
-#     uv run python ../../../fire360/explanations/compute_explanations.py --dataset_name dutch --model_name dutch_BB --top_k 1000 --synthetic_dataset_path /home/lcorbucci/fire360/artifacts/dutch/synthetic_data/synthetic_data_100000_epochs_2500_synthethizer_name_tvae.csv --bb_path /home/lcorbucci/fire360/artifacts/dutch/bb/dutch_BB.pth  --debug True --num_processes 20 --explanation_type logistic --validation_seed $i --store_path /home/lcorbucci/fire360/artifacts/dutch/explanations
-# done
+for i in $(seq 1 2);
+do
+    uv run python ../../../fire360/explanations/compute_explanations.py --dataset_name dutch --model_name dutch_BB --top_k 1000 --synthetic_dataset_path /home/lcorbucci/fire360/artifacts/dutch/synthetic_data/synthetic_data_100000_epochs_2500_synthethizer_name_tvae.csv --bb_path /home/lcorbucci/fire360/artifacts/dutch/bb/dutch_BB.pth  --debug True --num_processes 20 --explanation_type logistic --validation_seed $i --store_path /home/lcorbucci/fire360/artifacts/dutch/explanations
+done
 
 
 for i in $(seq 50 51);
